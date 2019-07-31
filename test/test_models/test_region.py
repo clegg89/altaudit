dc83335a4b05ccf0adcf19fe36a24b300339c37e
Casey Smith <clegg89@gmail.com>
2019-07-31 08:01:10 -0400

    Added create test

--- test/test_models/test_region.py
+++ test/test_models/test_region.py
@@ -16,3 +16,24 @@ def test_create_region_table():
     Region.__table__.create(engine)
     assert engine.has_table('regions')
     Region.__table__.drop(engine)
+    session.commit()
+    session.close()
+
+def test_add_region():
+    path = os.path.dirname(os.path.realpath(__file__))
+    engine = create_engine("sqlite:///{}/test.db".format(path))
+    Session = sessionmaker(engine)
+    session = Session()
+    Region.__table__.create(engine)
+
+    us = Region(name='US')
+    session.add(us)
+
+    assert us == session.query(Region).filter(Region.name=='US').first()
+
+    session.delete(us)
+    session.commit()
+
+    Region.__table__.drop(engine)
+    session.commit()
+    session.close()
