8fcf5276bc718a0d22cbaa4859343e4f6abdc317
Casey Smith <clegg89@gmail.com>
2019-08-02 16:39:11 -0400

    Fleshed out more of audit constructor

--- charfetch/audit.py
+++ charfetch/audit.py
@@ -4,6 +4,10 @@ import yaml
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from wowapi import WowApi
+
+from .models import Base, Class, Races
+
 class Audit:
     """
     Top-Level class Responsible for managing the system.
@@ -11,6 +15,28 @@ class Audit:
     This class will hold all necessary data across multiple refreshes
     """
 
-    def __init__(config_file):
+    def __init__(self, config_file):
         with open(config_file, 'r') as f:
             config = yaml.safe_load(f)
+
+        self.engine = create_engine(config['database'])
+        self.blizzard_api = WowApi(**config['api']['blizzard'])
+
+        self.server = config['server']
+
+        Base.metadata.create_all(self.engine)
+
+        session = sessionmaker(self.engine)()
+
+        session.query(Class).delete()
+        session.query(Race).delete()
+
+        classes = self.api.get_character_classes('us', locale='en_US')['classes']
+        races = self.api.get_character_races('us', locale='en_US')['races']
+
+        session.add_all([Class(c['name'], id=c['id']) for c in classes])
+        session.add_all([Race(r['name'], id=r['id']) for r in races])
+
+    def refresh(self):
+        "Refresh each character and write the result to the server"
+        pass
