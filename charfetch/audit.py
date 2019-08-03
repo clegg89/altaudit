ebeeaa20e9190c9f67ff789ebc4d1be773a46bbc
Casey Smith <clegg89@gmail.com>
2019-08-03 11:07:49 -0400

    Untested code for audit initialization

--- charfetch/audit.py
+++ charfetch/audit.py
@@ -28,6 +28,12 @@ class Audit:
 
         session = sessionmaker(self.engine)()
 
+        if session.query(Faction).count() != 3:
+            session.query(Faction).delete()
+            for i,v in ['alliance', 'horde', 'neutral']:
+                f = Faction(v, id=i)
+                session.add(f)
+
         session.query(Class).delete()
         session.query(Race).delete()
 
@@ -36,6 +42,42 @@ class Audit:
 
         session.add_all([Class(c['name'], id=c['id']) for c in classes])
         session.add_all([Race(r['name'], id=r['id']) for r in races])
+
+        config_characters = [{'name' : character, 'realm' : realm, 'region' : region}
+                for region,realms in config['characters'].items()
+                for realm,characters in realm.items()
+                for character in characters]
+
+        stored_characters = session.query(Character).all()
+
+        for char in stored_characters:
+            if not {'name' : char.name, 'realm' : char.realm_name, 'region' : char.region_name} in config_characters:
+                session.delete(char)
+
+        for region,realms in data.items():
+            region_model = session.query(Region).filter_by(name=region).first()
+            if not region_model:
+                region_model = Region(region)
+                session.add(region_model)
+
+            for realm,characters in realms.items():
+                realm_model = session.query(Realm).filter_by(name=realm).join(Region).filter_by(name=region).first()
+                if not realm_model:
+                    realm_model = Realm(realm, region_model)
+                    session.add(realm_model)
+
+                for character in characters:
+                    character_model = session.query(Character).\
+                            filter_by(name=character).\
+                            join(Realm).filtery_by(name=realm).\
+                            join(Region).filter_by(name=region).first()
+
+                    if not character_model:
+                        character_model = Character(character, realm=realm, region=region)
+                        session.add(character_model)
+
+        session.commit()
+        session.close()
 
     def refresh(self):
         "Refresh each character and write the result to the server"
