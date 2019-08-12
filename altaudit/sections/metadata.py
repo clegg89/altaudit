3f330e85cd3fbdacf0ff4df41e974be2e70e149c
Casey Smith <clegg89@gmail.com>
2019-08-11 20:26:38 -0400

    Added raids to metadata

--- altaudit/sections/metadata.py
+++ altaudit/sections/metadata.py
@@ -1,7 +1,8 @@
 """Metadata for the refresh call"""
 
 from ..utility import Utility
-from ..constants import VERSION, WOW_PATCH
+from ..constants import VERSION, WOW_PATCH, VALID_RAIDS
 
 def metadata():
-    return [Utility.refresh_time, VERSION, WOW_PATCH]
+    raids = '|'.join(['{}+{}'.format(raid['name'], '_'.join([boss['name'] for boss in raid['encounters']])) for raid in VALID_RAIDS])
+    return [Utility.refresh_time, VERSION, WOW_PATCH, raids]
--- test/test_sections/test_metadata.py
+++ test/test_sections/test_metadata.py
@@ -20,3 +20,6 @@ def test_metadata_software_version():
 
 def test_metadata_wow_patch():
     assert Section.metadata()[2] == WOW_PATCH
+
+def test_metadata_raids():
+    assert Section.metadata()[3] == "Uldir+Taloc_MOTHER_Fetid Devourer_Zek'voz, Herald of N'zoth_Vectis_Zul, Reborn_Mythrax the Unraveler_G'huun|Battle of Dazar'alor+Champion of the Light_Grong_Jadefire Masters_Opulence_Conclave of the Chosen_King Rastakhan_Mekkatorque_Stormwall Blockade_Lady Jaina Proudmoore|Crucible of Storms+The Restless Cabal_Uu'nat, Harbinger of the Void|The Eternal Palace+Abyssal Commander Sivara_Radiance of Azshara_Blackwater Behemoth_Lady Ashvane_Orgozoa_The Queen's Court_Za'qul_Queen Azshara"
