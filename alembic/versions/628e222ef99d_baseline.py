d1df3abdf3a154b35f8619c4d745189ccb08780e
Casey Smith <clegg89@gmail.com>
2019-08-13 08:01:44 -0400

    Added alembic upgrade scripts for most recent schema.
    run  to get to latest revision

---
+++ alembic/versions/628e222ef99d_baseline.py
@@ -0,0 +1,24 @@
+"""Baseline
+
+Revision ID: 628e222ef99d
+Revises:
+Create Date: 2019-08-12 17:56:52.644548
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '628e222ef99d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
