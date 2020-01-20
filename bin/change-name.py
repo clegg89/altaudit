1efbfa31c96178443f690c1df5bfb3ce737d8513
Casey Smith <clegg89@gmail.com>
2020-01-19 21:35:40 -0500

    Added working script

--- bin/change-name.py
+++ bin/change-name.py
@@ -11,22 +11,80 @@ Change the name of an existing character and update the database
 """
 import yaml
 import sys
-# import csv
-# from datetime import datetime
-# import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from altaudit.models import Region, Realm, Character
+
+## TODO:
+#
+# Add unit tests
+# Remove empty realms in config
 
 if __name__ == '__main__':
     with open('config.yaml', 'r') as f:
         config = yaml.safe_load(f)
 
     region = sys.argv[1]
-    realm = sys.argv[2]
+    realmIn = sys.argv[2]
     characterIn = sys.argv[3]
-    characterOut = sys.argv[4]
+    realmOut = sys.argv[4]
+    characterOut = sys.argv[5]
 
-    index = config['characters'][region][realm].index(characterIn)
 
-    config['characters'][region][realm][index] = characterOut
+    # If not changing realms, just update index
+    if realmOut == realmIn:
+        # Find the previous character in config
+        index = config['characters'][region][realmIn].index(characterIn)
+        config['characters'][region][realmOut][index] = characterOut
+    else:
+        # Different realms, delete old character and add new one
+        config['characters'][region][realmIn].remove(characterIn)
 
-    with open('out.yaml', 'w') as f:
-        yaml.dump(config, f)
+        # Create the realm if it doesn't exist
+        if realmOut not in config['characters'][region].keys():
+            config['characters'][region][realmOut] = list()
+
+        # Append to new list
+        config['characters'][region][realmOut].append(characterOut)
+
+    # Database stuff
+    engine = create_engine(config['database'], echo=True)
+    session = sessionmaker(engine)()
+
+    try:
+        # Find previous entry in db
+        character_model = session.query(Character).\
+                filter_by(name=characterIn).join(Realm).\
+                filter_by(name=realmIn).join(Region).\
+                filter_by(name=region).first()
+
+        # Find realm we're moving to
+        realm_out_model = session.query(Realm).\
+                filter_by(name=realmOut).join(Region).\
+                filter_by(name=region).first()
+
+        # Error, not found
+        if not character_model:
+            print("Character Not Found")
+            raise
+
+        # New realm
+        if not realm_out_model:
+            realm_out_model = Realm(realmOut, character_model.realm.region)
+            session.add(realm_out_model)
+
+        # Update
+        character_model.name = characterOut
+        character_model.realm = realm_out_model
+
+        # Update config
+        with open('config.yaml', 'w') as f:
+            yaml.dump(config, f)
+
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
