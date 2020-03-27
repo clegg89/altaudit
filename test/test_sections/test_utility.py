540baa86bc96b5687246c228581e3d2e5f55ab37
Casey Smith <clegg89@gmail.com>
2020-03-27 10:44:09 -0400

    Fixed

--- test/test_sections/test_utility.py
+++ test/test_sections/test_utility.py
@@ -8,8 +8,10 @@ def test_not_weapon_if_two_handed_not_fury_warrior():
             'summary' : {
                 'character_class' : { 'name' : 'Warlock' },
                 'active_spec' : { 'name' : 'Destruction' }},
-            'equipment' : { 'equipped_items' : [
-                { 'slot' : { 'type' : 'MAIN_HAND' }, 'inventory_type' : { 'type' : 'TWOHWEAPON' }}]}}
+            'equipment' : { 'equipped_items' : [{
+                'slot' : { 'type' : 'MAIN_HAND' },
+                'inventory_type' : { 'type' : 'TWOHWEAPON' },
+                'item_subclass' : { 'name' : 'Axe' }}]}}
 
     assert False == is_off_hand_weapon(profile)
 
@@ -18,8 +20,10 @@ def test_is_weapon_if_not_two_handed():
             'summary' : {
                 'character_class' : { 'name' : 'Rogue' },
                 'active_spec' : { 'name' : 'Outlaw' }},
-            'equipment' : { 'equipped_items' : [
-                { 'slot' : { 'type' : 'MAIN_HAND' }, 'inventory_type' : { 'type' : 'WEAPON' }}]}}
+            'equipment' : { 'equipped_items' : [{
+                'slot' : { 'type' : 'MAIN_HAND' },
+                'inventory_type' : { 'type' : 'WEAPON' },
+                'item_subclass' : { 'name' : 'Axe' }}]}}
 
     assert True == is_off_hand_weapon(profile)
 
@@ -28,8 +32,10 @@ def test_is_weapon_if_two_handed_and_fury_warrior():
             'summary' : {
                 'character_class' : { 'name' : 'Warrior' },
                 'active_spec' : { 'name' : 'Fury' }},
-            'equipment' : { 'equipped_items' : [
-                { 'slot' : { 'type' : 'MAIN_HAND' }, 'inventory_type' : { 'type' : 'TWOHWEAPON' }}]}}
+            'equipment' : { 'equipped_items' : [{
+                'slot' : { 'type' : 'MAIN_HAND' },
+                'inventory_type' : { 'type' : 'TWOHWEAPON' },
+                'item_subclass' : { 'name' : 'Axe' }}]}}
 
     assert True == is_off_hand_weapon(profile)
 
@@ -46,7 +52,22 @@ def test_active_spec_is_missing():
     profile = {
             'summary' : {
                 'character_class' : { 'name' : 'Warrior' }},
-            'equipment' : { 'equipped_items' : [
-                { 'slot' : { 'type' : 'MAIN_HAND' }, 'inventory_type' : { 'type' : 'TWOHWEAPON' }}]}}
+            'equipment' : { 'equipped_items' : [{
+                'slot' : { 'type' : 'MAIN_HAND' },
+                'inventory_type' : { 'type' : 'TWOHWEAPON' },
+                'item_subclass' : { 'name' : 'Axe' }}]}}
+
+    assert False == is_off_hand_weapon(profile)
+
+@pytest.mark.parametrize("subclass", ['Bow', 'Crossbow', 'Gun'])
+def test_ranged_hunter_weapons_no_offhand(subclass):
+    profile = {
+            'summary' : {
+                'character_class' : { 'name' : 'Hunter' },
+                'active_spec' : { 'name' : 'Marksmanship' }},
+            'equipment' : { 'equipped_items' : [{
+                'slot' : { 'type' : 'MAIN_HAND' },
+                'inventory_type' : { 'type' : 'WEAPON' },
+                'item_subclass' : { 'name' : subclass }}]}}
 
     assert False == is_off_hand_weapon(profile)
